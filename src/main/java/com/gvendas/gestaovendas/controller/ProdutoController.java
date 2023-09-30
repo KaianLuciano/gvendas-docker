f3c0b67625ad80436d7e8a9c0d32fe5a6f556bc9
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-09-30 16:11:28 -0300

    Criado findAll e findById para ProdutoController

--- src/main/java/com/gvendas/gestaovendas/controller/ProdutoController.java
+++ src/main/java/com/gvendas/gestaovendas/controller/ProdutoController.java
@@ -1,9 +1,31 @@
 package com.gvendas.gestaovendas.controller;
 
+import com.gvendas.gestaovendas.model.Produto;
+import com.gvendas.gestaovendas.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/produto")
+@AllArgsConstructor
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        return ResponseEntity.ok().body(produtoService.findAll());
+    }
+
+    @GetMapping("/{idProduto}")
+    public ResponseEntity<Produto> findById(@PathVariable(value = "idProduto") Long idProduto) {
+        return ResponseEntity.ok().body(produtoService.findById(idProduto));
+    }
 }
--- src/main/java/com/gvendas/gestaovendas/service/ProdutoService.java
+++ src/main/java/com/gvendas/gestaovendas/service/ProdutoService.java
@@ -1,7 +1,24 @@
 package com.gvendas.gestaovendas.service;
 
+import com.gvendas.gestaovendas.exceptions.DadoNaoEncontradoException;
+import com.gvendas.gestaovendas.model.Produto;
+import com.gvendas.gestaovendas.repository.ProdutoRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto findById(Long idProduto) {
+        return produtoRepository.findById(idProduto).orElseThrow(() -> new DadoNaoEncontradoException("Produto com o id " + idProduto + " não encontrado."));
+    }
 }
