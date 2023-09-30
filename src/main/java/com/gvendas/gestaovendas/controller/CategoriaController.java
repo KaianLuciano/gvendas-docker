53ea650e6fc09d18df594cd1eabe6acac64bfde0
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-09-30 15:50:03 -0300

    Criado classes Controller, Service e Repository para as entidades Produto e Categoria

---
+++ src/main/java/com/gvendas/gestaovendas/controller/CategoriaController.java
@@ -0,0 +1,9 @@
+package com.gvendas.gestaovendas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+}
