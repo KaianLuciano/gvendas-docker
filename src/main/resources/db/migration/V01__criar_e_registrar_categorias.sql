1f53e5bf2c6401383375d928380d0972d8aef495
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-09-30 15:28:35 -0300

    Mudado versao mysql docker.yml e atualizado migration V02

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,10 +1,10 @@
-CREATE TABLE categoria {
-    codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE categoria (
+                           codigo BIGINT PRIMARY KEY AUTO_INCREMENT,
+                           nome VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
-INSERT INTO categoria (nome) values ('Tecnologia')
-INSERT INTO categoria (nome) values ('Acessórios para Veículos')
-INSERT INTO categoria (nome) values ('Esporte e Lazer')
-INSERT INTO categoria (nome) values ('Casa e eletrodomésticos')
-INSERT INTO categoria (nome) values ('Joias e Relógios')
+INSERT INTO categoria (nome) values ('Tecnologia');
+INSERT INTO categoria (nome) values ('Acessórios para veículos');
+INSERT INTO categoria (nome) values ('Esporte e Lazer');
+INSERT INTO categoria (nome) values ('Casa e Eletrodomésticos');
+INSERT INTO categoria (nome) values ('Joias e Relógios');
